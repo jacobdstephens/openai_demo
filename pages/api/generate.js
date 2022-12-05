3fa2c8e1955eba34230787439ffcfb2427effefd
Sobek Battle <sobekbattle@protonmail.com>
2022-12-05 06:47:43 -0800

    Initial Commit, copyting over working code from local.

---
+++ pages/api/generate.js
@@ -0,0 +1,31 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+  const completion = await openai.createCompletion({
+    model: "text-davinci-002",
+    prompt: req.body.question,
+    max_tokens: 2000,
+    temperature: 0.6,
+    stop: "---END---",
+  });
+  res.status(200).json({ response: completion.data });
+
+}
+
+function generatePrompt(animal) {
+  const capitalizedAnimal =
+    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
+  return `Suggest three names for an animal that is a superhero.
+
+Animal: Cat
+Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+Animal: Dog
+Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+Animal: ${capitalizedAnimal}
+Names:`;
+}
